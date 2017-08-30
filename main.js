05dbb25dd1b87c94280b9fc91dcd044d130190ae
Ayoub <hajjem.ayoub@gmail.com>
2017-08-30 17:26:06 +0200

    simple text fulfillment action: action.simple-text

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-exports.requestHandler = (req, res) => {
-  console.log('main request handler called');
-  res.send('hello world');
+process.env.DEBUG = 'actions-on-google:*';
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+
+exports.requestHandler = (request, response) => {
+  const app = new ApiAiApp({request, response});
+  let actionMap = new Map();
+  //TODO change action.simple-text with the actions name you specified in API.ai
+  actionMap.set('action.simple-text', simpleTextAction);
+  app.handleRequest(actionMap);
+};
+
+const simpleTextAction = (app) => {
+  app.ask( "Hello" );
 };
