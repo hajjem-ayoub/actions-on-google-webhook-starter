b3f982457c5106d779a7956db47767b9d256a6c6
Ayoub <hajjem.ayoub@gmail.com>
2017-08-30 18:14:55 +0200

    custom payload response

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ process.env.DEBUG = 'actions-on-google:*';
 const ApiAiApp = require('actions-on-google').ApiAiApp;
 
 exports.requestHandler = (request, response) => {
-  const app = new ApiAiApp({request, response});
+  const app = new ApiAiApp({ request, response });
   let actionMap = new Map();
   //TODO change action.simple-text with the actions name you specified in API.ai
   actionMap.set('action.simple-text', simpleTextAction);
+  actionMap.set('action.custom-payload', customPayloadAction);
   app.handleRequest(actionMap);
 };
 
 const simpleTextAction = (app) => {
-  app.ask( "Hello" );
+  app.ask("Hello from simple text action");
+};
+
+const customPayloadAction = (app) => {
+  app.response_.send(
+    { 
+      "speech": "Hello from custom text action"
+    }
+  );
 };
