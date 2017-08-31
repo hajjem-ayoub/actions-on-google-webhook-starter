8aa71aa97b53d7cfdada88678bbbd05126e2d6b9
Ayoub <hajjem.ayoub@gmail.com>
2017-08-31 20:13:38 +0200

    add simpleRichResponseAction

--- main.js
+++ main.js
@@ -7,7 +7,23 @@ exports.requestHandler = (request, response) => {
   //TODO change action.simple-text with the actions name you specified in API.ai
   actionMap.set('action.simple-text', simpleTextAction);
   actionMap.set('action.custom-payload', customPayloadAction);
+  actionMap.set('action.simple-rich-response', simpleRichResponseAction);
   app.handleRequest(actionMap);
+};
+
+const simpleRichResponseAction = (app) => {
+  app.ask(app.buildRichResponse()
+    // Create a basic card and add it to the rich response
+    .addSimpleResponse('Look at that pizza')
+    .addBasicCard(app.buildBasicCard(`This is a paragraph describing that buitiful pizza.  
+    2 spaces on the row means a new line when i just go to new line in the code 
+    this is not going to be another new line neither does \n.  you are limited to 10 line limit with an image, 
+    15 line limit without an image. `)
+      .setTitle('This is the titile of the pizza')
+      .addButton('And a button')
+      .setImage(`${DOMAIN_URL}/pizza_card.jpg`, 'Image alternate text')
+    )
+  );
 };
 
 const simpleTextAction = (app) => {
